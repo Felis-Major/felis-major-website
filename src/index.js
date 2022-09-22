e7c59b296863bf2625689d1505339b023d92a02f
Robert Edilber <robert.edilber@pm.me>
2022-09-22 15:41:10 +0200

    Added github pages deployment capabilities

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
 // CSS Import
 import './index.css'; // Main StyleSheet
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>
+	</HashRouter>
 );
